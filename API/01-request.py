8d3f30a69ab7db8975b008484cb8a2c9ff023a35
Marcelo Almeida <marceloalmeidacrm@gmail.com>
2025-03-29 23:54:49 -0300

    Testing public API with GET

--- API/01-request.py
+++ API/01-request.py
@@ -1,1 +1,38 @@
+
+from pprint import pprint
 import requests
+
+
+def obter_request(url, params=None):
+    """ Faz uma requisição  GET e retorna a resposta em JSON"""
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+    except requests.HTTPError as e:
+        print(f"Erro no request: {e}")
+        return None
+
+def busca_id_estado():
+    """ Obtém um dicionario de estados no formato {id_estado: nome_estado}"""
+    
+    url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+    dados_estados = obter_request(url, params={"view": "nivelado"}) or []
+    return {estado["UF-id"]: estado["UF-nome"] for estado in dados_estados} 
+
+def frequencia_nome(name):
+    """Obtém um dicionario de frequencia de um nome por estado no formato{id_estado: frequencia}"""
+    url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{name}"
+    dados_frequencia = obter_request(url, params={"groupBy": "UF"}) or []
+    return {int(dado["localidade"]): dado["res"][0]["proporcao"] for dado in dados_frequencia}
+
+def main(name):
+    dict_estados = busca_id_estado() 
+    dict_frequencias = frequencia_nome(name)
+    print(f"=== Frequência do nome {name} nos Estados (por 100.000 habitantes) entre os anos 2013 à 2023 ===")
+    for id_estado, frequencia in sorted(dict_frequencias.items(), key=lambda item: item[1], reverse=True):
+        print(f"-> {dict_estados.get(id_estado, 'Desconhecido')}: {frequencia}")   
+
+
+if __name__ == "__main__":
+    main("Marcelo")
